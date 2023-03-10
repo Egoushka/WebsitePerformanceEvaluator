e8673fd7a0e0e7c3d61b70414ac59e3034a49251
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-03-10 15:56:21 +0200

    Added filter by scheme

--- WebsitePerformanceEvaluator.Core/Extensions/LinkFilterExtension.cs
+++ WebsitePerformanceEvaluator.Core/Extensions/LinkFilterExtension.cs
@@ -6,16 +6,20 @@ public static class LinkFilterExtension
     {
         var uri = new Uri(baseUrl);
 
-
         return links
             .Distinct()
             .Select(link => new Uri(uri, link))
+            .RemoveNotHttpsOrHttpScheme()
             .CheckLinksHosts(uri)
             .RemoveAnchorLinks()
             .Select(link => link.AbsoluteUri)
             .CheckForSlashAndRemove();
     }
 
+    public static IEnumerable<Uri> RemoveNotHttpsOrHttpScheme(this IEnumerable<Uri> links)
+    {
+        return links.Where(link => link.Scheme == Uri.UriSchemeHttp || link.Scheme == Uri.UriSchemeHttps);
+    }
     private static IEnumerable<string> CheckForSlashAndRemove(this IEnumerable<string> links)
     {
         return links.Select(link => link.EndsWith("/") ? link.Remove(link.Length - 1) : link);
