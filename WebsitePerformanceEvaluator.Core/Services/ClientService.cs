0089bcf3c66f96794ef596941493cb04c8070abe
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-03-11 19:37:25 +0200

    Refactoring

--- WebsitePerformanceEvaluator.Core/Services/ClientService.cs
+++ WebsitePerformanceEvaluator.Core/Services/ClientService.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Diagnostics;
 using HtmlAgilityPack;
 using Serilog;
@@ -10,7 +11,6 @@ public class ClientService : IClientService
 {
     private readonly HttpClient _httpClient;
     private readonly ILogger _logger;
-    static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(500);
     public ClientService(ILogger logger, IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
@@ -19,43 +19,57 @@ public class ClientService : IClientService
 
     public async Task<IEnumerable<string>> CrawlWebsiteToFindLinks(string url)
     {
-        const int semaphoreCount = 8;
         _logger.Information("Start getting links by crawling");
-        var semaphoreSlim = new SemaphoreSlim(semaphoreCount);
         var links = new HashSet<string> { url };
         var visitedLinks = new List<string>();
         var linksToVisit = new List<string> { url };
+        
         while (linksToVisit.Count > 0)
         {
-            var tasks = new List<Task>();
-            for (var i = 0; i < linksToVisit.Count; i++)
-            {
-                var link = linksToVisit[i];
-                linksToVisit.RemoveAt(i);
-                visitedLinks.Add(link);
-                await semaphoreSlim.WaitAsync();
-
-                var task = Task.Run(async () =>
-                {
-                    var newLinks = (await CrawlPageToFindLinks(link)).ApplyFilters(url).ToList();
-                    links.UnionWith(newLinks);
-                    foreach (var newLink in newLinks
-                                 .Where(newLink =>
-                                     !visitedLinks.Contains(newLink) && !linksToVisit.Contains(newLink)))
-                    {
-                        linksToVisit.Add(newLink);
-                    }
-                    
-                    semaphoreSlim.Release();
-                    
-                    return Task.CompletedTask;
-                });
-                tasks.Add(task);
-            }
+            var tasks = GetCrawlingTasks(linksToVisit, links, visitedLinks, url);
+            
             await Task.WhenAll(tasks);
         }
 
         return links;
+    }
+    private IEnumerable<Task> GetCrawlingTasks(List<string> linksToVisit, ISet<string> links, 
+        ICollection<string> visitedLinks, string url)
+    {
+        const int semaphoreCount = 8;
+        
+        var tasks = new List<Task>();
+        var semaphoreSlim = new SemaphoreSlim(semaphoreCount);
+        
+        for (var i = 0; i < linksToVisit.Count; i++)
+        {
+            var link = linksToVisit[i];
+            linksToVisit.RemoveAt(i);
+            visitedLinks.Add(link);
+            
+            semaphoreSlim.Wait();
+
+            var task = Task.Run(async () =>
+            {
+                var newLinks = (await CrawlPageToFindLinks(link)).ApplyFilters(url).ToList();
+                var filteredNewLinks = FilterNewLinks(newLinks, visitedLinks, linksToVisit);
+                
+                links.UnionWith(newLinks);
+                linksToVisit.AddRange(filteredNewLinks);
+                
+                semaphoreSlim.Release();
+                
+                return Task.CompletedTask;
+            });
+            tasks.Add(task);
+        }
+        return tasks;
+    }
+    private IEnumerable<string> FilterNewLinks(IEnumerable<string> newLinks, ICollection<string> visitedLinks, 
+        ICollection<string> linksToVisit)
+    {
+        return newLinks.Where(newLink =>
+            !visitedLinks.Contains(newLink) && !linksToVisit.Contains(newLink));
     }
     public async Task<IEnumerable<string>> CrawlPageToFindLinks(string url)
     {
@@ -86,20 +100,18 @@ public class ClientService : IClientService
 
     public int GetTimeResponse(string url)
     {
-        var timer = new Stopwatch();
-
-        timer.Start();
+        var timeNow = DateTime.Now;
+        
         try
         {
             _httpClient.GetAsync(url).Wait();
         }
         catch (Exception e)
         {
-            _logger.Error(e, "Error while getting response");
+            _logger.Error(e, "Error while getting response time");
         }
-        timer.Stop();
-
-        var timeTaken = timer.Elapsed;
+        
+        var timeTaken = DateTime.Now - timeNow;
 
         return timeTaken.Milliseconds;
     }
