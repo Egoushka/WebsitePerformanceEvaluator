a35cfa179c2d79a345fbba638e005b994088dc06
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-07 16:31:28 +0300

    Added tests for HtmlParser

---
+++ WebsitePerformanceEvaluator.Core.Tests/Parsers/HtmlParserTests.cs
@@ -0,0 +1,79 @@
+using HtmlAgilityPack;
+using Moq;
+using WebsitePerformanceEvaluator.Core.Models;
+using WebsitePerformanceEvaluator.Core.Models.Enums;
+using WebsitePerformanceEvaluator.Core.Parsers;
+using WebsitePerformanceEvaluator.Core.Service;
+using Xunit;
+
+namespace WebsitePerformanceEvaluator.Core.Tests.Parsers;
+
+
+public class HtmlParserTests
+{
+    private readonly Mock<HttpClientService> _httpClientServiceMock;
+    private readonly HtmlParser _htmlParser;
+
+    public HtmlParserTests()
+    {
+        _httpClientServiceMock = new Mock<HttpClientService>();
+        _htmlParser = new HtmlParser(_httpClientServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task GetLinksAsync_Should_Return_List_With_Single_Link_When_Client_Returns_Null()
+    {
+        // Arrange
+        var url = "https://example.com";
+        _httpClientServiceMock.Setup(service => service.GetDocumentAsync(It.IsAny<LinkPerformance>()))
+            .ReturnsAsync(new HtmlDocument());
+
+        // Act
+        var result = (await _htmlParser.GetLinksAsync(url)).ToList();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.Equal(url, result[0].Link);
+        Assert.Equal(CrawlingLinkSource.Website, result[0].CrawlingLinkSource);
+    }
+
+    [Fact]
+    public async Task GetLinksAsync_Should_Return_List_With_Single_Link_When_No_Links_Are_Found()
+    {
+        // Arrange
+        var url = "https://example.com";
+        _httpClientServiceMock.Setup(service => service.GetDocumentAsync(It.IsAny<LinkPerformance>()))
+            .ReturnsAsync(new HtmlDocument());
+
+        // Act
+        var result = (await _htmlParser.GetLinksAsync(url)).ToList();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.Equal(url, result[0].Link);
+        Assert.Equal(CrawlingLinkSource.Website, result[0].CrawlingLinkSource);
+    }
+
+    [Fact]
+    public async Task GetLinksAsync_Should_Return_List_When_Links_Found()
+    {
+        //Arrange
+        var url = "https://example.com";
+        
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml("<html><body><a href=\"https://example.com/1\">1</a><a href=\"https://example.com/2\">2</a></body></html>");
+        
+        _httpClientServiceMock.Setup(service => service.GetDocumentAsync(It.IsAny<LinkPerformance>()))
+            .ReturnsAsync(htmlDocument);
+        
+        //Act
+        var result = (await _htmlParser.GetLinksAsync(url)).ToList();
+        
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(3, result.Count);
+    }
+
+}
