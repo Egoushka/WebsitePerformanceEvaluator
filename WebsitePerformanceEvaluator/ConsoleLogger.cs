3a3a362c25f5b5b86e2ac1907bdabbdd9509b257
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-07 10:23:59 +0300

    Removed unused logger functions

--- WebsitePerformanceEvaluator/ConsoleLogger.cs
+++ WebsitePerformanceEvaluator/ConsoleLogger.cs
@@ -4,28 +4,8 @@ namespace WebsitePerformanceEvaluator;
 
 public class ConsoleLogger : ILogger
 {
-    public void Debug(string message)
-    {
-        Console.WriteLine($"[DEBUG] {message}");
-    }
-
-    public void Information(string message)
-    {
-        Console.WriteLine($"[INFO] {message}");
-    }
-
-    public void Warning(string message)
-    {
-        Console.WriteLine($"[WARNING] {message}");
-    }
-
     public void Error(string message)
     {
         Console.WriteLine($"[ERROR] {message}");
-    }
-
-    public void Fatal(string message)
-    {
-        Console.WriteLine($"[FATAL] {message}");
     }
 }
