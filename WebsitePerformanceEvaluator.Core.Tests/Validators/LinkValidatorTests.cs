9023afa57c9410d1994417562ec5119506caebfe
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-07 16:36:52 +0300

    Added tests for LinkValidator

---
+++ WebsitePerformanceEvaluator.Core.Tests/Validators/LinkValidatorTests.cs
@@ -0,0 +1,53 @@
+using WebsitePerformanceEvaluator.Core.Validators;
+using Xunit;
+
+namespace WebsitePerformanceEvaluator.Core.Tests.Validators;
+
+public class LinkValidatorTests
+{
+    private readonly LinkValidator _linkValidator;
+
+    public LinkValidatorTests()
+    {
+        _linkValidator = new LinkValidator();
+    }
+
+    [Fact]
+    public void IsValidLink_WithAnchor_ReturnsFalse()
+    {
+        // Arrange
+        var link = "https://www.example.com/#about";
+
+        // Act
+        var result = _linkValidator.IsValidLink(link);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsValidLink_WithAttributes_ReturnsFalse()
+    {
+        // Arrange
+        var link = "https://www.example.com/?search=123";
+
+        // Act
+        var result = _linkValidator.IsValidLink(link);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsValidLink_ValidLink_ReturnsTrue()
+    {
+        // Arrange
+        var link = "https://www.example.com/about";
+
+        // Act
+        var result = _linkValidator.IsValidLink(link);
+
+        // Assert
+        Assert.True(result);
+    }
+}
