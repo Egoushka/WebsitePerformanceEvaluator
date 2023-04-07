376976f37bd3ca4210b5eb3a6d623dc7bdb1ae2f
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-07 15:20:21 +0300

    Added virtual modifier

--- WebsitePerformanceEvaluator.Core/Validators/LinkValidator.cs
+++ WebsitePerformanceEvaluator.Core/Validators/LinkValidator.cs
@@ -2,7 +2,7 @@ namespace WebsitePerformanceEvaluator.Core.Validators;
 
 public class LinkValidator
 {
-    public bool IsValidLink(string link)
+    public virtual bool IsValidLink(string link)
     {
         var doesNotContainAnchor = !link.Contains('#');
         var doesNotContainAttributes = !link.Contains('?');
