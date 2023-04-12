88d5b1aa750372feef22937a218a75ce7b9c8beb
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-12 11:40:55 +0300

    yield -> collection return

--- WebsitePerformanceEvaluator.Core.Tests/Crawlers/Common/CrawlersUtils.cs
+++ WebsitePerformanceEvaluator.Core.Tests/Crawlers/Common/CrawlersUtils.cs
@@ -7,25 +7,31 @@ public static class CrawlersUtils
 {
     public static IEnumerable<LinkPerformance> GetExpectedLinks(CrawlingLinkSource source, int count)
     {
+        var links = new List<LinkPerformance>();
         for (var i = 0; i < count; i++)
         {
-            yield return new LinkPerformance
+            links.Add(new LinkPerformance
             {
                 CrawlingLinkSource = source,
                 Link = $"https://example.com/{i}",
                 TimeResponseMs = 100
-            };
+            });
         }
+
+        return links;
     }
     public static IEnumerable<LinkPerformance> GetExpectedLinksWithoutTimeResponse(CrawlingLinkSource source, int count)
     {
+        var response = new List<LinkPerformance>();
         for (var i = 0; i < count; i++)
         {
-            yield return new LinkPerformance
+            response.Add(new LinkPerformance
             {
                 CrawlingLinkSource = source,
                 Link = $"https://example.com/{i}"
-            };
+            });
         }
+
+        return response;
     }
 }
