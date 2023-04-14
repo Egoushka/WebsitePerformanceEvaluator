3776be56bab44026f4dfd7b9f73a773ba3c9f3e2
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-14 13:31:35 +0300

    Added GetHashCode and Equals to Core model

--- WebsitePerformanceEvaluator.Core/Models/LinkPerformance.cs
+++ WebsitePerformanceEvaluator.Core/Models/LinkPerformance.cs
@@ -7,4 +7,14 @@ public class LinkPerformance
     public string Link { get; set; }
     public int? TimeResponseMs { get; set; }
     public CrawlingLinkSource CrawlingLinkSource { get; set; }
+    
+    public override int GetHashCode()
+    {
+        return Link.GetHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is LinkPerformance link && link.Link == Link;
+    }
 }
