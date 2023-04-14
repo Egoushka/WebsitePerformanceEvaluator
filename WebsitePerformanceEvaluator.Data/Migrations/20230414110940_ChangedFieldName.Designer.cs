22be8b9312afd3bda5bf1f9012025d118dad8881
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-14 14:38:31 +0300

    Updated database

--- WebsitePerformanceEvaluator.Data/Migrations/20230414110940_ChangedFieldName.Designer.cs
+++ WebsitePerformanceEvaluator.Data/Migrations/20230414110940_ChangedFieldName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebsitePerformanceEvaluator.Data;
 
@@ -11,9 +12,11 @@ using WebsitePerformanceEvaluator.Data;
 namespace WebsitePerformanceEvaluator.Data.Migrations
 {
     [DbContext(typeof(WebsitePerformanceEvaluatorDatabaseContext))]
-    partial class WebsitePerformanceEvaluatorDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230414110940_ChangedFieldName")]
+    partial class ChangedFieldName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebsitePerformanceEvaluator.Data/Migrations/20230414110940_ChangedFieldName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebsitePerformanceEvaluator.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedFieldName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Link",
+                table: "LinkPerformances",
+                newName: "Url");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "LinkPerformances",
+                newName: "Link");
+        }
+    }
+}
