2550ecfd6398e5605f6deefdf2c78c18e51c9c25
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-14 17:11:36 +0300

    Added redirecting to page with link performances

--- WebsitePerformanceEvaluator.MVC/Controllers/LinksController.cs
+++ WebsitePerformanceEvaluator.MVC/Controllers/LinksController.cs
@@ -8,22 +8,28 @@ namespace WebsitePerformanceEvaluator.MVC.Controllers
 {
     public class LinksController : Controller
     {
+        private readonly ILinkRepository _linkRepository;
         private readonly ILinkPerformanceRepository _linkPerformanceRepository;
         private readonly Crawler _crawler;
 
-        public LinksController(ILinkPerformanceRepository linkPerformanceRepository, Crawler crawler)
+        public LinksController(ILinkRepository linkRepository, ILinkPerformanceRepository linkPerformanceRepository,
+            Crawler crawler)
         {
+            _linkRepository = linkRepository;
             _linkPerformanceRepository = linkPerformanceRepository;
             _crawler = crawler;
         }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var links = _linkRepository.GetAll();
+
+            return View(links);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(string url)
+        public async Task<IActionResult> GetLinksFromUrl(string url)
         {
             var links = await _crawler.CrawlWebsiteAndSitemapAsync(url);
 
@@ -40,7 +46,15 @@ namespace WebsitePerformanceEvaluator.MVC.Controllers
                 Link = link,
             });
 
-            return View(linkPerformances);
+            await _linkRepository.AddAsync(link);
+            await _linkPerformanceRepository.AddRangeAsync(linkPerformances);
+
+            return Index();
+        }
+
+        public IActionResult LinkPerformance(int linkId)
+        {
+            return RedirectToAction("Index", "LinkPerformance", new {linkId});
         }
     }
 }
