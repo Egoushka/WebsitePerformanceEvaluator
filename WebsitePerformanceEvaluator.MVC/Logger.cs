dff35222f1af60713774d07ac59840e2b3981bad
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-14 16:25:17 +0300

    Added logger

---
+++ WebsitePerformanceEvaluator.MVC/Logger.cs
@@ -0,0 +1,10 @@
+using ILogger = WebsitePerformanceEvaluator.Core.Interfaces.ILogger;
+
+namespace WebsitePerformanceEvaluator.MVC;
+
+public class Logger : ILogger
+{
+    public void Error(string message)
+    {
+    }
+}
