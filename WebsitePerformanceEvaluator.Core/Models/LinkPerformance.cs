a693caa7a0ab48889fdff45df316f251a7f2675f
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-14 12:21:07 +0300

    Removed Id from core LinkPerformance data class

--- WebsitePerformanceEvaluator.Core/Models/LinkPerformance.cs
+++ WebsitePerformanceEvaluator.Core/Models/LinkPerformance.cs
@@ -4,7 +4,6 @@ namespace WebsitePerformanceEvaluator.Core.Models;
 
 public class LinkPerformance
 {
-    public int Id { get; set; }
     public string Link { get; set; }
     public int? TimeResponseMs { get; set; }
     public CrawlingLinkSource CrawlingLinkSource { get; set; }
