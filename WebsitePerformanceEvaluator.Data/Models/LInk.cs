e9dccd1e98bdfb67bc3f9df04823ce82bd2a0885
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-14 13:23:40 +0300

    Added navigation properties for Data classes

--- WebsitePerformanceEvaluator.Data/Models/LInk.cs
+++ WebsitePerformanceEvaluator.Data/Models/LInk.cs
@@ -4,4 +4,6 @@ public class Link
 {
     public int Id { get; set; }
     public string Url { get; set; }
+    
+    public virtual ICollection<LinkPerformance> LinkPerformances { get; set; }
 }
--- WebsitePerformanceEvaluator.Data/Models/LinkPerformance.cs
+++ WebsitePerformanceEvaluator.Data/Models/LinkPerformance.cs
@@ -5,7 +5,10 @@ namespace WebsitePerformanceEvaluator.Data.Models;
 public class LinkPerformance
 {
     public int Id { get; set; }
+    public int LinkId { get; set; }
     public string Link { get; set; }
     public int? TimeResponseMs { get; set; }
     public CrawlingLinkSource CrawlingLinkSource { get; set; }
+    
+    public virtual Link LinkNavigation { get; set; }
 }
