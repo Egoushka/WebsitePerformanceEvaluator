b6b2ef33a88e828d77ba35b59173a9fa4a37a941
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-18 10:54:03 +0300

    Added Link view model

---
+++ WebsitePerformanceEvaluator.MVC/ViewModels/LinkViewModel.cs
@@ -0,0 +1,11 @@
+using WebsitePerformanceEvaluator.Data.Models;
+
+namespace WebsitePerformanceEvaluator.MVC.ViewModels;
+
+public class LinkViewModel
+{
+    public IEnumerable<Link> Links { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+}
